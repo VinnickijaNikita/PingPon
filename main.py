50e69a04b1533ccfb78ec6cb5b542b4d55337bdc
Jeff <115884919+VinnickijaNikita@users.noreply.github.com>
2023-04-22 19:04:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
 from pygame import *
+from button import Button
 
 window = display.set_mode((700,500))
+clock = time.Clock()
+
+game = True
+
+btn_start = Button(200,200, 100, 50, "start_btn.png")
+btn_end = Button(200,400, 100, 50, "exit_btn.png")
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if run:
+        
+        window.fill((255,0,0))
+    else:
+        window.fill((0,255,0))
+        if btn_start.draw(window):
+            run = True
+        if btn_end.draw(window):
+            game = False
 
 
-class GameSprite(sprite.Sprite):
-    def __init__(self, image_name, x, y, width, height, speed):
-        super().__init__()
-        self.image = transform.scale(image.load(image_name), (width, height))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-    
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
-
-class Player(GameSprite):
-    def update_1(self):
-        keys_pressed = key.get_pressed()
-
-        if keys[K_w] :
-            self.rect.y -= self.speed
-        if keys[K_s] :
-            self.rect.y += self.speed
-
-    def update_r(self):
-        keys = key.get_pressed()
-
-        if keys[K_UP] :
-            self.rect.y -= self.speed
-        if keys[K_DOWN] :
-            self.rect.y += self.speed
-
-player1 = Player("Table.png", 20, 200, 50, 200, 5)
-player2 = Player("Table.png", 500, 200, 50, 200, 5)
-ball = GameSprite("ball.png", 200, 200, 50, 200, 5)
-
+    display.update()
+    clock.tick(60)
